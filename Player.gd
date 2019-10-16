26513388a7a565f13b85dca764f78b72c499bf88
Marco <mrcgll98@gmail.com>
2019-10-16 15:03:51 +0200

    Basic scenes setup

--- Player.gd
+++ Player.gd
@@ -1,8 +1,17 @@
 extends Node2D
 
+signal player_shot
+
+export var bullet_scene : PackedScene 
+
 func _ready() -> void:
 	pass
 
 func _process(delta: float) -> void:
 	$Cannon.rotation_degrees = rad2deg($Cannon.position.angle_to_point(get_local_mouse_position())) + 90
 	pass
+
+func _on_Shot_pressed() -> void:
+	emit_signal("player_shot")
+	var bullet = bullet_scene.instance()
+	add_child(bullet)
