2c7803ac115e571d81cdbc5be1659deb4f54684b
Marco <mrcgll98@gmail.com>
2019-10-19 13:35:29 +0200

    Fixed cannon rotation

--- src/Bullet.gd
+++ src/Bullet.gd
@@ -1,4 +1,4 @@
-extends Area2D
+extends RigidBody2D
 
 func _ready() -> void:
 	pass
--- src/Player.gd
+++ src/Player.gd
@@ -3,14 +3,18 @@ extends Node2D
 export var bullet_scene : PackedScene
 var bullet_spawn_pos : Vector2
 
-
 func _input(event: InputEvent) -> void:
-	if event is InputEventScreenTouch:
-		var angle = $Cannon.get_angle_to(get_global_mouse_position()) + PI/2
-		$Tween.interpolate_property($Cannon, "rotation", $Cannon.rotation,
-			$Cannon.rotation + angle, 0.5, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
-		$Tween.start()
-		print($Cannon.rotation)
+    if event is InputEventScreenTouch:
+        var angle = $Cannon.get_angle_to(get_global_mouse_position()) + PI/2
+        if angle > PI:
+            if get_global_mouse_position().x < get_viewport_rect().size.x / 2: 
+                angle = $Cannon.get_angle_to(get_global_mouse_position()) - 3*PI/2
+            if get_global_mouse_position().x > get_viewport_rect().size.x / 2: 
+                angle = $Cannon.get_angle_to(get_global_mouse_position()) + PI/2
+        $Tween.interpolate_property($Cannon, "rotation", $Cannon.rotation,
+            $Cannon.rotation + angle, 0.5, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
+        $Tween.start()
+        print($Cannon.rotation)
 
 func _on_Shot_pressed() -> void:
 	bullet_spawn_pos = $Cannon/BulletSpawnPos.global_position
