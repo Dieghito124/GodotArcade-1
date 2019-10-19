993b338870beb59dc3026fcac3d92d24f5eceb69
Marco <mrcgll98@gmail.com>
2019-10-19 16:27:58 +0200

    Started to work on the laser

--- src/Player.gd
+++ src/Player.gd
@@ -4,23 +4,33 @@ export var bullet_scene : PackedScene
 var bullet_spawn_pos : Vector2
 
 func _input(event: InputEvent) -> void:
-    if event is InputEventScreenTouch:
-        var angle = $Cannon.get_angle_to(get_global_mouse_position()) + PI/2
-        if angle > PI:
-            if get_global_mouse_position().x < get_viewport_rect().size.x / 2: 
-                angle = $Cannon.get_angle_to(get_global_mouse_position()) - 3*PI/2
-            if get_global_mouse_position().x > get_viewport_rect().size.x / 2: 
-                angle = $Cannon.get_angle_to(get_global_mouse_position()) + PI/2
-        $Tween.interpolate_property($Cannon, "rotation", $Cannon.rotation,
-            $Cannon.rotation + angle, 2, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
-        $Tween.start()
+	if event is InputEventScreenTouch:
+		var angle = $Cannon.get_angle_to(get_global_mouse_position()) + PI/2
+		if angle > PI:
+			if get_global_mouse_position().x < get_viewport_rect().size.x / 2: 
+				angle = $Cannon.get_angle_to(get_global_mouse_position()) - 3*PI/2
+			if get_global_mouse_position().x > get_viewport_rect().size.x / 2: 
+				angle = $Cannon.get_angle_to(get_global_mouse_position()) + PI/2
+		$Tween.interpolate_property($Cannon, "rotation", $Cannon.rotation,
+			$Cannon.rotation + angle, 2, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
+		$Tween.start()
 
-func _on_Shot_pressed() -> void:
-    bullet_spawn_pos = $Cannon/BulletSpawnPos.global_position
-    Events.emit_signal("player_shot", bullet_spawn_pos)
+func _draw() -> void:
+	pass
+
+func _player_shot() -> void:
+	bullet_spawn_pos = $Cannon/BulletSpawnPos.global_position
+	Events.emit_signal("player_shot", bullet_spawn_pos)
+	_play_shot_sound()
 
 func _on_Tween_all_completed() -> void:
-    pass #Stop the FX sounds
+	$Laser.add_point($Cannon.position)
+	$Laser.add_point(get_global_mouse_position())
+	pass #Stop the FX sounds
 
 func _on_Tween_started(object: Object, key: NodePath) -> void:
-    pass #Start the FX sounds
+	pass #Start the FX sounds
+
+func _play_shot_sound() -> void:
+	$Cannon/CannonShot.pitch_scale = rand_range(0.8, 1.2)
+	$Cannon/CannonShot.play()
