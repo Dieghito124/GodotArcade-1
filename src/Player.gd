10f837c20104be2f746f72fefd9bd1b65d107cf6
Marco Galli <mrcgll98@gmail.com>
2019-10-20 16:56:32 +0200

    Revert "Tank Shot (not complete)"

--- src/Player.gd
+++ src/Player.gd
@@ -12,23 +12,16 @@ func _input(event: InputEvent) -> void:
 			if get_global_mouse_position().x > get_viewport_rect().size.x / 2: 
 				angle = $Cannon.get_angle_to(get_global_mouse_position()) + PI/2
 		$Tween.interpolate_property($Cannon, "rotation", $Cannon.rotation,
-			$Cannon.rotation + angle, 0.5, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
+			$Cannon.rotation + angle, 2, Tween.TRANS_SINE, Tween.EASE_IN_OUT)
 		$Tween.start()
-		print($Cannon.rotation)
 
-func _on_Shot_pressed() -> void:
-	#bullet_spawn_pos = $Cannon/BulletSpawnPos.global_position
-	#Events.emit_signal("player_shot", bullet_spawn_pos)
-	var sc_bul = load("res://src/Bullet.tscn")
-	var bul = sc_bul.instance()
-	get_node("/root/Game/World1").add_child(bul)
-	bul.position = $Cannon/BulletSpawnPos.position
+func _draw() -> void:
+	pass
 
-func _process(delta):
-	Events.ang = $Cannon.rotation
-	if Events.shot == true:
-		_on_Shot_pressed()
-
+func _player_shot() -> void:
+	bullet_spawn_pos = $Cannon/BulletSpawnPos.global_position
+	Events.emit_signal("player_shot", bullet_spawn_pos)
+	_play_shot_sound()
 
 func _on_Tween_all_completed() -> void:
 	$Laser.add_point($Cannon.position)
